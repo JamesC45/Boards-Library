601d5fc113b8b01ec66936b7a9bf901790626420
James <FortnieGangSigma345345@outlook.com>
2024-06-09 14:41:30 -0700

    Add files via upload
    Added files

---
+++ Plugin/LibraryItsSelf/Board.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine.UI;
+using UnityEngine;
+
+namespace BoardsLibrary.Plugin.LibraryItsSelf
+{
+    internal class Board
+    {
+        public static void Setcoc(string COCTOP, string COCBOTTOM, Color col)
+        {
+            var Objects = new[]
+            {
+                GameObject.Find("CodeOfConduct"), // Top
+                GameObject.Find("COC Text"), // Bottom
+                GameObject.Find("Environment Objects/LocalObjects_Prefab/TreeRoom/forestatlas (combined by EdMeshCombinerSceneProcessor)") // bg
+            };
+            Objects[0].GetComponent<Text>().text = COCTOP;
+            Objects[1].GetComponent<Text>().text = COCBOTTOM;
+            Material gdg = new Material(Shader.Find("GorillaTag/UberShader"))
+            {
+                color = col,
+            };
+            Objects[2].GetComponent<MeshRenderer>().material = gdg;
+        }
+        public static void SetMOTD(string Top, string bottom)
+        {
+            var objects = new[]{
+                GameObject.Find("Environment Objects/LocalObjects_Prefab/TreeRoom/TreeRoomInteractables/UI/motd"), // TOP
+                GameObject.Find("Environment Objects/LocalObjects_Prefab/TreeRoom/TreeRoomInteractables/UI/motd/motdtext"), // BOTTOM
+            };
+            objects[0].GetComponent<Text>().text = Top;
+            objects[1].GetComponent<Text>().text = bottom;
+        }
+
+    }
+}
---
+++ Plugin/Load.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using UnityEngine;
+
+namespace BoardsLibrary
+{
+    [BepInPlugin("AZORA", "BOARDSCONFIG", "1.0.0")]
+    public class Load : BaseUnityPlugin
+    {
+        public void Update()
+        {
+            // Updating boards every frame
+
+            // strings
+            var list = new List<string>()
+            {
+                "BOARDS LIBRARY", // MOTD TOP
+                "BOARDS LIBRARY", // COC TOP
+                "this is a modification for gorilla tag that allows you to change the boards as you like.", // COC BOTTOM
+                "FOLLOW MY GITHUB :PRAY: - (github.com/JamesC45)", // MOTD BOTTOM
+            };
+            var Colors = new List<Color>() // this is for motd and coc btw.
+            {
+                Color.black, // Color 1
+                new Color(0.2f, 0.2f, 0.2f, 1f) // Color 2
+            };
+            BoardsLibrary.Plugin.LibraryItsSelf.Board.Setcoc(list[1], list[2].ToUpper(), Color.Lerp(Colors[0], Colors[1], Mathf.PingPong(Time.time, 1f)));
+            BoardsLibrary.Plugin.LibraryItsSelf.Board.SetMOTD(list[0], list[3].ToUpper());
+        }
+
+    }
+}
